861ef362e5b7783777d51820ee9991589f6201a1
Viorel Soltan <joomjoom2011@gmail.com>
2025-11-11 22:38:18 +0200

    cng files

---
+++ wp-docker/plugins/core-wpland/core-wpland.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Core WPLand
+ * Description: (scurtă descriere)
+ * Version: 0.1
+ * Author: Viorel Soltan
+ * Text Domain: core-wpland
+ * Domain Path: /languages
+ */
+
+
+
+
+// Your plugin code starts here
+define('WPLAND_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('WPLAND_PLUGIN_PATH', plugin_dir_path(__FILE__));
